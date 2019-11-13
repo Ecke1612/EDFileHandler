1df84f16b0dde7f63317aa05b695eec2bb92b0d0
Eike Dreyer <eikedreyer@gmx.de>
2019-11-13 22:09:58 +0100

    PlainHandler Namensänderungen

--- src/main/java/com/ed/filehandler/JsonHandler.java
+++ src/main/java/com/ed/filehandler/JsonHandler.java
@@ -1,5 +1,6 @@
 package com.ed.filehandler;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -22,12 +23,40 @@ public class JsonHandler {
         }
     }
 
+    public void writeJsonArrayData(JSONArray array, String path) {
+        try (FileWriter file = new FileWriter(path)) {
+
+            file.write(array.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public JSONObject readJsonData(String path) {
         JSONParser jsonParser = new JSONParser();
         try (FileReader reader = new FileReader(path))
         {
             JSONObject obj = (JSONObject) jsonParser.parse(reader);
             return obj;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public JSONArray readJsonArrayData(String path) {
+        JSONParser jsonParser = new JSONParser();
+        try (FileReader reader = new FileReader(path))
+        {
+            JSONArray array = (JSONArray) jsonParser.parse(reader);
+            return array;
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
